9962907f1dd3882495c0548e7872e9b8b22df870
Yuri Dogandjiev <yuri.dogandjiev@live.com>
2019-12-10 10:59:56 -0800

    Add weather info

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.get("*", (req, res) => {
   res.sendFile("build/index.html", { root: __dirname });
 });
 
-var port = process.env.port || 3000;
+var port = process.env.port || 3001;
 app.listen(port, function () {
   console.log('Listening on http://localhost:' + port);
 });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    if (localStorage["data"]) {
+      setData(localStorage["data"]);
+    };
+
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Seattle,US&units=imperial&APPID=d0cb21f45c215358b09cc3c09b5ef147"
+    )
+      .then(res => res.json())
+      .then(data => {
+        localStorage["data"] = data;
+        setData(data);
+      })
+      .catch(err => setData({ error: err }));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      {data ? (
+        <div>
+          <div>
+            {data.main && data.main.temp && (
+              <div>
+                <div>Temp: {data.main.temp}</div>
+                <div>High: {data.main.temp_max}</div>
+                <div>Low: {data.main.temp_min}</div>
+                <div>Conditions: {data.weather[0].main}</div>
+                <div>Location: {data.name}</div>
+                <div>Updated: {new Date(data.dt).toString()}</div>
+              </div>
+            )}
+          </div>
+          <div>
+            {data.error && (
+              <div>Error: {data.error.toString()}</div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
